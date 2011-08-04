4b6b16abfcb80c7f55712a7a46e6d059d7c2348f
antis <antis1990@gmail.com>
2011-08-04 15:14:10 +0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,42 +3,61 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
+  before(:each) do
+	@base_title = "Ruby on Rails Tutorial Sample App"
+	@success = "should be successful"
+	@right_title = "should have the right title"
+  end
+
   describe "GET 'home'" do
-    it "should be successful" do
+    it @success do
       get 'home'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it @right_title do
       get 'home'
       response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | Home")
+                    :content => @base_title + " | Home")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it @success do
       get 'contact'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it @right_title do
       get 'contact'
       response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | Contact")
+                    :content => @base_title + " | Contact")
     end
   end
 
   describe "GET 'about'" do
-    it "should be successful" do
+    it @success do
       get 'about'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it @right_title do
       get 'about'
       response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | About")
+                    :content => @base_title + " | About")
+    end
+  end
+
+ describe "GET 'help'" do
+    it @success do
+      get 'help'
+      response.should be_success
+    end
+
+    it @right_title do
+      get 'help'
+      response.should have_selector("title",
+                    :content => @base_title + " | Help")
     end
   end
 end
